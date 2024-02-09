105d2fbdd15848fe26245224cd12d370207a7586
Prakashh16 <mishraprakash1230@gmail.com>
2024-02-09 09:11:07 +0530

    add

---
+++ TestConfig/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+
+namespace TestConfig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"I am {ConfigurationManager.AppSettings["Name"]} working on {ConfigurationManager.AppSettings["CompanyName"]}");
+            Console.ReadLine();
+        }
+    }
+}
